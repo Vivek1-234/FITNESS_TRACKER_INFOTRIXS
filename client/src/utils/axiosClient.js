90ff8eccbfa845774c7de220b46946f403e6cfce
SmartChallenge <91767879+Vivek1-234@users.noreply.github.com>
2023-08-28 01:30:30 +0530

    Again Hashrouter add

--- client/src/utils/axiosClient.js
+++ client/src/utils/axiosClient.js
@@ -13,9 +13,8 @@ import { setLoading, showToast } from "../redux/slices/appConfigSlice";
 import { TOAST_FAILURE } from "../App";
 
 let baseURL = 'http://localhost:4000/';
-if(process.env.NODE_ENV === 'production') {
-    baseURL = process.env.REACT_APP_SERVER_BASE_URL
-} 
+baseURL = process.env.REACT_APP_SERVER_BASE_URL
+
 export const axiosClient = axios.create({
   baseURL,
   withCredentials: true,
