999272a5ea40b5fd4e414bb0d9c3047d59f1d1b5
SmartChallenge <91767879+Vivek1-234@users.noreply.github.com>
2023-08-28 02:28:08 +0530

    remove client_origin

--- client/src/utils/axiosClient.js
+++ client/src/utils/axiosClient.js
@@ -6,15 +6,13 @@ import {
   KEY_ACCESS_TOKEN,
   removeItem,
   setItem,
-} from "./localStorageManager";  
+} from "./localStorageManager";
 
-import  store from '../redux/store';
+import store from "../redux/store";
 import { setLoading, showToast } from "../redux/slices/appConfigSlice";
 import { TOAST_FAILURE } from "../App";
 
-let baseURL = '';
 export const axiosClient = axios.create({
-  baseURL,
   withCredentials: true,
 });
 
@@ -26,55 +24,59 @@ axiosClient.interceptors.request.use((request) => {
 });
 
 axiosClient.interceptors.response.use(
- 
   async (respone) => {
     store.dispatch(setLoading(false));
     const data = respone.data;
-    if (data.status === "ok") {             
+    if (data.status === "ok") {
       return data;
     }
 
     const originalRequest = respone.config;
     const statusCode = data.statusCode;
-    const error = data.message
+    const error = data.message;
 
+    store.dispatch(
+      showToast({
+        type: TOAST_FAILURE,
+        message: error,
+      })
+    );
 
-    store.dispatch(showToast({
-      type: TOAST_FAILURE,
-      message: error
-  }))
-    
     if (statusCode === 401 && !originalRequest._retry) {
       // means the access token has expired
       originalRequest._retry = true;
 
-      const response = await axios.create({
+      const response = await axios
+        .create({
           withCredentials: true,
         })
-        .get(`${baseURL}/auth/refresh`);
+        .get(`/auth/refresh`);
 
       if (response.data.status === "ok") {
         setItem(KEY_ACCESS_TOKEN, response.data.result.accessToken);
         originalRequest.headers[
           "Authorization"
-        ] = `Bearer ${response.data.result.accessToken}`;   
+        ] = `Bearer ${response.data.result.accessToken}`;
 
         return axios(originalRequest);
       } else {
         removeItem(KEY_ACCESS_TOKEN);
         window.location.replace("/", "_self");
         window.location.reload(true);
-       
+
         return Promise.reject(error);
       }
     }
     return Promise.reject(error);
-  },   async(error) => {
-      store.dispatch(setLoading(false));
-      store.dispatch(showToast({
-          type: TOAST_FAILURE,
-          message: error.message
-      }))
-      return Promise.reject(error);
-    });
- 
+  },
+  async (error) => {
+    store.dispatch(setLoading(false));
+    store.dispatch(
+      showToast({
+        type: TOAST_FAILURE,
+        message: error.message,
+      })
+    );
+    return Promise.reject(error);
+  }
+);
--- index.js
+++ index.js
@@ -1,68 +1,65 @@
-const express= require('express');
-const dotenv = require('dotenv'); 
-const dbconnect = require('./dbconnect'); 
-const authRouter = require('./routers/authRouter');
-const workoutRouter = require('./routers/workoutRouter'); 
-const mealRouter = require('./routers/mealRouter'); 
-const morgan = require('morgan');  
-const { success } = require('./utils/responseWrapper');
-const cookieParser = require('cookie-parser'); 
-const cors = require('cors'); 
-const userRouter =  require('./routers/userRouter');
+/** @format */
+
+const express = require("express");
+const dotenv = require("dotenv");
+const dbconnect = require("./dbconnect");
+const authRouter = require("./routers/authRouter");
+const workoutRouter = require("./routers/workoutRouter");
+const mealRouter = require("./routers/mealRouter");
+const morgan = require("morgan");
+const { success } = require("./utils/responseWrapper");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const userRouter = require("./routers/userRouter");
 const cloudinary = require("cloudinary").v2;
-const path = require('path'); 
+const path = require("path");
 
-dotenv.config('./.env'); 
+dotenv.config("./.env");
 
-
-cloudinary.config({  
-    secure: true,  
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+cloudinary.config({
+  secure: true,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+const app = express();
 
-const  app = express(); 
-
-// middlewares                           
+// middlewares
 app.use(express.json({ limit: "50mb" }));
 
-app.use(morgan('common')); 
-app.use(cookieParser()); 
-
-
- let origin = process.env.CLIENT_ORIGIN;
+app.use(morgan("common"));
+app.use(cookieParser());
 
 
 app.use(
-    cors({
-        credentials: true,
-        origin
-    })
+  cors({
+    credentials: true,
+  })
 );
-app.use('/auth' , authRouter); 
-app.use('/workouts' , workoutRouter)
-app.use('/meals' , mealRouter)
-app.use("/user", userRouter); 
+app.use("/auth", authRouter);
+app.use("/workouts", workoutRouter);
+app.use("/meals", mealRouter);
+app.use("/user", userRouter);
 
 // app.get('/' , (req , res )=>{
 
 // return res.send(success(200, 'Ok from server'));
-// }); 
+// });
 
-app.use(express.static(path.join(__dirname , './client/build')))
- 
-app.get('*' , function(_ , res ){
-   res.sendFile(path.join(__dirname , "./client/build/index.html") , function(error){
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (error) {
       res.status(500).send(error);
-   })
-})  
-const PORT = process.env.PORT || 4001;  
+    }
+  );
+});
+const PORT = process.env.PORT || 4001;
 
-dbconnect();   
-app.listen( PORT, () =>{
-    console.log(`Listening on PORT :${PORT}`);  
-}); 
-
-
+dbconnect();
+app.listen(PORT, () => {
+  console.log(`Listening on PORT :${PORT}`);
+});
